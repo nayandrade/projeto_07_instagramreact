2d00eae651087b4a7e40bd981465c942bb61e985
Nayara <nrdeandrade@gmail.com>
2022-05-06 17:46:12 -0300

    Chore: Componentização

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar";
+import Corpo from "./Corpo";
+import FundoMobile from "./FundoMobile";
+
+
+export default function App () {
+    return (
+        <div>
+            <Corpo />
+            <Navbar />
+            <FundoMobile />
+        </div>
+        
+    )
+    
+}
